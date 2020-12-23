7be1f7dadf71c24d2b54143af7f30fe0b392eb2a
Chris Law <claw@spindogs.com>
2020-12-23 17:32:44 +0000

    Initial commit of proj so far

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <h2>MY mates</h2>
+        <h4>a few</h4>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Iglesias',
+                    phone: '1235345 23432',
+                    email: 'manuel@iglesias.esp'
+                },
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,45 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ this.detailsAreVisible ? 'Hide' : 'Show' }} details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Iglesias',
+                phone: '1235345 23432',
+                email: 'manuel@iglesias.esp'
+            },
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
+
+
+<style lang="scss">
+button {
+    background: green;
+    &::before {
+        content: '';
+        width: 5px;
+        height: 15px;
+        background: blue;
+    }
+}
+</style>
